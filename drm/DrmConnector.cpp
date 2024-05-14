ff7ecb0bb9e067b27d1f2c7846e9299577480ba1
manxiaoliang <xiaoliangx.man@intel.com>
2024-05-14 04:48:55 +0000

    Add some properties
    Multi-plane support
    1. Multi-plane can be enabled by property
       vendor.hwcomposer.planes.enabling.
       By default, only one plane is used if the property is not set.
       vendor.hwcomposer.planes.enabling=1 #support all planes
       vendor.hwcomposer.planes.enabling=0 #only support PRIMARY by default
    2. Select preferred mode
       If the property 'vendor.hwcomposer.preferred.mode.limit' does not
       exist, or set to '1' then only support preferred mode.
       If the property 'vendor.hwcomposer.preferred.mode.limit' is set to
       '0', support all modes.
       vendor.hwcomposer.preferred.mode.limit=1#only support preferred mode
       vendor.hwcomposer.preferred.mode.limit=0#support all modes
    3. Select specific mode
       This function is controlled by 2 properties.
          vendor.hwcomposer.connector.id
          vendor.hwcomposer.mode.id
       If the properties 'vendor.hwcomposer.connector.id' and
       'vendor.hwcomposer.mode.id' are all set, then only support the
       selected mode indicated by 'vendor.hwcomposer.mode.id'.
       If any one of the properties 'vendor.hwcomposer.connector.id' and
       'vendor.hwcomposer.mode.id' is not set, then to check the property
       'vendor.hwcomposer.preferred.mode.limit'.
          vendor.hwcomposer.connector.id=117
          vendor.hwcomposer.mode.id=2
    4. With these properties, hwc can provide flexibility for performance
       tuning and bug triage.
    5. Find mode id
    cat /sys/kernel/debug/dri/0/i915_display_info.The output like below:
    [CONNECTOR:117:DP-4]: status: connected
    modes:
    "1920x1080": 62 144500 1920 1968 2000 2080 1080 1083 1088 1112 0x48 0x9
    "1920x1080": 60 148500 1920 2008 2052 2200 1080 1084 1089 1125 0x40 0xa
    "1680x1050": 60 146250 1680 1784 1960 2240 1050 1053 1059 1089 0x40 0x6
    "1680x945": 60 131481 1680 1784 1960 2240 945 946 949 978 0x0 0x6
    mode id starts from 1, and increases with a step of 1 in order.
    mode id is 1:
    "1920x1080": 62 144500 1920 1968 2000 2080 1080 1083 1088 1112 0x48 0x9
    mode id is 2:
    "1920x1080": 60 148500 1920 2008 2052 2200 1080 1084 1089 1125 0x40 0xa
    mode id is 3:
    "1680x1050": 60 146250 1680 1784 1960 2240 1050 1053 1059 1089 0x40 0x6
    mode id is 4:
    "1680x945": 60 131481 1680 1784 1960 2240 945 946 949 978 0x0 0x6
    6. The property can be set in /vendor/build.prop.
    
    Tracked-On: OAM-118492
    Signed-off-by: manxiaoliang <xiaoliangx.man@intel.com>

--- drm/DrmConnector.cpp
+++ drm/DrmConnector.cpp
@@ -20,6 +20,7 @@
 
 #include <xf86drmMode.h>
 
+#include <cutils/properties.h>
 #include <array>
 #include <cerrno>
 #include <cstdint>
@@ -168,6 +169,7 @@ std::string DrmConnector::GetName() const {
 }
 
 int DrmConnector::UpdateModes() {
+  drm_->ResetModeId();
   auto conn = MakeDrmModeConnectorUnique(*drm_->GetFd(), GetId());
   if (!conn) {
     ALOGE("Failed to get connector %d", GetId());
@@ -175,19 +177,113 @@ int DrmConnector::UpdateModes() {
   }
   connector_ = std::move(conn);
 
-  modes_.clear();
+  int32_t connector_id;
+  int32_t mode_id;
+
+  char property[PROPERTY_VALUE_MAX];
+  memset(property, 0 , PROPERTY_VALUE_MAX);
+  property_get("vendor.hwcomposer.connector.id", property, "-1");
+  connector_id = atoi(property);
+  ALOGD("The property 'vendor.hwcomposer.connector.id' value is %d", connector_id);
+
+  memset(property, 0 , PROPERTY_VALUE_MAX);
+  property_get("vendor.hwcomposer.mode.id", property, "-1");
+  mode_id = atoi(property);
+  ALOGD("The property 'vendor.hwcomposer.mode.id' value is %d", mode_id);
+
+  bool preferred_mode_found = false;
+  std::vector<DrmMode> new_modes;
+
+  if (mode_id <= 0 || mode_id > connector_->count_modes)
+    mode_id = -1;
+
+  bool have_preferred_mode = false;
   for (int i = 0; i < connector_->count_modes; ++i) {
+    if (connector_->modes[i].type & DRM_MODE_TYPE_PREFERRED) {
+      have_preferred_mode = true;
+      break;
+    }
+  }
+
+  for (int i = 0; i < connector_->count_modes; ++i) {
+    if (drm_->preferred_mode_limit_ && connector_id == -1) {
+      if (have_preferred_mode) {
+        if (!(connector_->modes[i].type & DRM_MODE_TYPE_PREFERRED)) {
+          drm_->GetNextModeId();
+          continue;
+        }
+      } else {
+        have_preferred_mode = true;
+      }
+    }
+    if (connector_->connector_id == connector_id) {
+      if (mode_id == -1) {
+        if (drm_->preferred_mode_limit_) {
+          if (have_preferred_mode) {
+            if (!(connector_->modes[i].type & DRM_MODE_TYPE_PREFERRED)) {
+              drm_->GetNextModeId();
+              continue;
+            }
+          } else {
+            have_preferred_mode = true;
+         }
+        }
+      } else {
+        if (mode_id != (i + 1)) {
+          drm_->GetNextModeId();
+          continue;
+        }
+      }
+    } else {
+      if (connector_id != -1) {
+        if (drm_->preferred_mode_limit_) {
+          if (have_preferred_mode) {
+            if (!(connector_->modes[i].type & DRM_MODE_TYPE_PREFERRED)) {
+              drm_->GetNextModeId();
+              continue;
+            }
+          } else {
+            have_preferred_mode = true;
+          }
+        }
+      }
+    }
+
     bool exists = false;
     for (const DrmMode &mode : modes_) {
       if (mode == connector_->modes[i]) {
+        new_modes.push_back(mode);
         exists = true;
+        ALOGD("CONNECTOR:%d select one mode, id = %d, name = %s, refresh = %d",
+              GetId(), mode.GetId(), mode.GetName().c_str(), mode.GetRawMode().vrefresh);
         break;
       }
     }
 
     if (!exists) {
-      modes_.emplace_back(&connector_->modes[i]);
+      DrmMode m(&connector_->modes[i]);
+      m.SetId(drm_->GetNextModeId());
+      new_modes.push_back(m);
+      ALOGD("CONNECTOR:%d select one mode, id = %d, name = %s, refresh = %d",
+            GetId(), m.GetId(), m.GetName().c_str(), m.GetRawMode().vrefresh);
     }
+    if (!preferred_mode_found &&
+        (new_modes.back().GetRawMode().type & DRM_MODE_TYPE_PREFERRED)) {
+      preferred_mode_id_ = new_modes.back().GetId();
+      preferred_mode_found = true;
+      ALOGD("CONNECTOR:%d preferred mode found, set preferred mode id = %d, name "
+            "= %s, refresh = %d",
+          GetId(), preferred_mode_id_, new_modes.back().GetName().c_str(),
+          new_modes.back().GetRawMode().vrefresh);
+    }
+  }
+  modes_.swap(new_modes);
+  if (!preferred_mode_found && !modes_.empty()) {
+    preferred_mode_id_ = modes_[0].GetId();
+    ALOGD("CONNECTOR:%d preferred mode not found, set preferred mode id = %d, "
+          "name = %s, refresh = %d",
+        GetId(), preferred_mode_id_, modes_[0].GetName().c_str(),
+        modes_[0].GetRawMode().vrefresh);
   }
 
   return 0;
